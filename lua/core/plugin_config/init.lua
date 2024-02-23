86c698985488eb3e45426dec87f0a7b4e6b3c0d3
ZERO PACKET LOSS <stanleyogada2000@gmail.com>
2024-02-23 10:49:57 +0100

    Configure gitsigns plugin

--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -3,3 +3,4 @@ require("core.plugin_config.lualine");
 require("core.plugin_config.nvim-tree");
 require("core.plugin_config.telescope");
 require("core.plugin_config.treesitter");
+require("core.plugin_config.gitsigns");
