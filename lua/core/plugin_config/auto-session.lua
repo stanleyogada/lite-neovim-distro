a1610e16c4e12191de021b2821084d6f149b8be8
ZERO PACKET LOSS <stanleyogada2000@gmail.com>
2024-02-24 16:02:56 +0100

    Have auto-session use Telescope to switch sessions

--- lua/core/plugin_config/auto-session.lua
+++ lua/core/plugin_config/auto-session.lua
@@ -1,4 +1,15 @@
 require("auto-session").setup({
     log_level = "error",
     auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+
+  session_lens = {
+    buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+    load_on_setup = true,
+    theme_conf = { border = true },
+    previewer = false,
+  },
 });
+
+vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {
+  noremap = true,
+})
