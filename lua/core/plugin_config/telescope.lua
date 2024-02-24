bdf508a553d78b339a73c498820f2a17bc980769
ZERO PACKET LOSS <stanleyogada2000@gmail.com>
2024-02-24 14:55:51 +0100

    Add Telescope keymaps for good whic-keys

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,6 +1,45 @@
-local builtin = require('telescope.builtin');
+require("telescope").setup({
+  defaults = {
+    prompt_prefix = "🔍 ",
+    selection_caret = "❯ ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "descending",
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        mirror = false,
+      },
+      vertical = {
+        mirror = false,
+      },
+    },
+    file_sorter = require("telescope.sorters").get_fzy_sorter,
+    file_ignore_patterns = {},
+    generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+    path_display = {},
+    winblend = 0,
+    border = {},
+    borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+    color_devicons = true,
+    use_less = true,
+    set_env = { ["COLORTERM"] = "truecolor" },
+    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+  },
+  extensions = {
+    fzy_native = {
+      override_generic_sorter = false,
+      override_file_sorter = true,
+    },
+  },
+})
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {});
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {});
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {});
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {});
+
+
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true });
+vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true });
+vim.keymap.set('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true });
+vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true });
