ae33f07ea0f3a1072542d0b94892f1baa9006ecb
ZERO PACKET LOSS <stanleyogada2000@gmail.com>
2024-02-26 07:40:53 +0100

    Fix NvimTree blank on vim open

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -3,6 +3,10 @@ vim.g.loaded_netrwPlugin = 1;
 
 require("nvim-tree").setup();
 
+
+
+vim.cmd([[autocmd VimLeavePre * lua vim.cmd(":NvimTreeFindFileToggle")]])
 vim.cmd([[ NvimTreeFindFileToggle ]]);
+
 vim.keymap.set('n', '<leader>n', ':NvimTreeFindFileToggle<CR>');
 
