0077edec3835f3594c75e1141951f8d7b93f9018
ZERO PACKET LOSS <stanleyogada2000@gmail.com>
2024-02-26 07:43:31 +0100

    Have NvimTree window Closed on vim open

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -6,7 +6,6 @@ require("nvim-tree").setup();
 
 
 vim.cmd([[autocmd VimLeavePre * lua vim.cmd(":NvimTreeFindFileToggle")]])
-vim.cmd([[ NvimTreeFindFileToggle ]]);
 
 vim.keymap.set('n', '<leader>n', ':NvimTreeFindFileToggle<CR>');
 
