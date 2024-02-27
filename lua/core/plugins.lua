882acd08526c440f30c4a75c9591dfb7e1087821
ZERO PACKET LOSS <stanleyogada2000@gmail.com>
2024-02-27 20:34:28 +0100

    Remove tralling space

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -24,7 +24,7 @@ return require('packer').startup(function(use)
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     requires = {
-      "debugloop/telescope-undo.nvim" 
+      "debugloop/telescope-undo.nvim"
     },
   }
   use "lewis6991/gitsigns.nvim"
